3d598afdb499c97d3ec0b73e41448b509639eeb5
Chanya-cloud <neetishachanya@gmail.com>
2026-04-16 07:45:07 +0000

    task 8

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,10 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 int main(void)
 {
-    printf("%d\n", _abs(-98));
-    printf("%d\n", _abs(0));
-    printf("%d\n", _abs(98));
+    jack_bauer();
     return (0);
 }
